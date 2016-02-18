7a0252f13ac72585ff0b63aa38e11be7406e2126
phani <phani05353@gmail.com>
2016-02-18 20:06:32 +0000

    done with CRUD and deploy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only:[:edit,:update,:destroy,:show]
 
    def index
 
@@ -8,7 +9,7 @@ class ArticlesController < ApplicationController
     @article=Article.new
   end
   def edit
-    @article=Article.find(params[:id])
+
   end
 
   def create
@@ -24,7 +25,7 @@ class ArticlesController < ApplicationController
    end
   end
   def update
-     @article=Article.find(params[:id])
+
      if  @article.update(article_params)
      flash[:notice] = "Your article is updated"
      redirect_to article_path(@article)
@@ -33,10 +34,10 @@ class ArticlesController < ApplicationController
    end
   end
   def show
-    @article=Article.find(params[:id])
+
   end
   def destroy
-     @article=Article.find(params[:id])
+
     @article.destroy
 
     flash[:notice] = "Your article is deleted"
@@ -47,7 +48,9 @@ class ArticlesController < ApplicationController
   end
 
   private
-
+  def set_article
+    @article=Article.find(params[:id])
+  end
   def article_params
     params.require(:article).permit(:title,:description)
   end
