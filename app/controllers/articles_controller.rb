405e8354bb404db9e9f37438dea87fdf2bf43e39
phani <phani05353@gmail.com>
2016-02-20 20:41:48 +0000

    add user migration

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,18 +12,16 @@ class ArticlesController < ApplicationController
 
   end
 
-  def create
-    #render plain: params[:article].inspect
-    @article=Article.new(article_params)
-
-
-   if  @article.save
-     flash[:notice] = "Your article is created"
-     redirect_to article_path(@article)
-     else
-     render 'new'
-   end
-  end
+ def create
+      @article = Article.new(article_params)
+      @article.user = User.first
+   if @article.save
+      flash[:success] = "Article was successfully created"
+      redirect_to article_path(@article)
+      else
+       render 'new'
+      end
+ end
   def update
 
      if  @article.update(article_params)
