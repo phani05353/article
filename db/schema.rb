0460f74d6d275dd8d8456574637cd925470ce9e6
phani05353 <phani05353@gmail.com>
2016-02-23 00:55:48 +0000

    article category asscociation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160222204638) do
+ActiveRecord::Schema.define(version: 20160223001738) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
